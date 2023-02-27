c617e78f0f4e03b81e85f76ff9e77b8eaa9afda2
feltbad <>
2023-02-27 19:30:41 +0300

    add reverse feature

--- firstproj/views.py
+++ firstproj/views.py
@@ -6,3 +6,8 @@ def about(request):
 
 def home(request):
     return render(request, 'home.html', {'greeting':'Hello home page'})
+
+def reverse(request):
+    text_area = request.GET['textarea']
+    reversed_text = text_area[::-1]
+    return render(request, 'reverse.html', {'textarea':text_area, 'reversedtext':reversed_text})
